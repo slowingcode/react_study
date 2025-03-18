8094625883723642c2c94b6660169c7e9304656a
slowingcode <wlghks0134@gmail.com>
2025-03-18 17:28:02 +0900

    feat: 댓글 props 코드 추가2

--- my-app/src/chapter_05/CommentList.jsx
+++ my-app/src/chapter_05/CommentList.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Comment from "./Comment";
 
 function CommentList(props) {
-    return (
-        <div>
-            <Comment />
-        </div>
-    );
+  return (
+    <div>
+      <Comment name={"김철수"} comment={"안녕하세요, 수플입니다."} />
+      <Comment name={"콩진호호"} comment={"안녕하세요. 안녕하세요요."} />
+    </div>
+  );
 }
 
 export default CommentList;
