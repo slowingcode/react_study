64cfe53b02d2a89e0fcc84c4e669a9b976b6ae1c
slowingcode <wlghks0134@gmail.com>
2025-03-19 00:31:12 +0900

    feat: state 활용

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //import App from "./App";
-import './index.css';
+import "./index.css";
 //import reportWebVitals from './reportWebVitals';// eslint-disable-line no-unused-vars
 
-import Clock from './chapter_04/Clock';
-import CommenList from './chapter_05/CommentList';
+import Clock from "./chapter_04/Clock";
+import CommenList from "./chapter_05/CommentList";
+import NotificationList from "./chapter_06/NotificationList";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(() =>{
+const root = ReactDOM.createRoot(document.getElementById("root"));
+setInterval(() => {
   root.render(
     <React.StrictMode>
-      <CommenList />
-    </React.StrictMode>,);
+      <NotificationList />
+    </React.StrictMode>
+  );
 }, 1000);
 
 // If you want to start measuring performance in your app, pass a function
