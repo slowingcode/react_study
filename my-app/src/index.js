6539ae4974e549bd51fcd23d1111c3a8521bb60b
slowingcode <wlghks0134@gmail.com>
2025-03-18 15:01:34 +0900

    first commit

---
+++ my-app/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';// eslint-disable-line no-unused-vars
+
+import Clock from './chapter_04/Clock';
+
+setInterval(() =>{
+  ReactDOM.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}, 1000);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
