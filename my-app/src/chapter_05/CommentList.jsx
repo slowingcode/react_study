055f67995fa680af83b51169142add8bf5e350d6
slowingcode <wlghks0134@gmail.com>
2025-03-18 17:32:31 +0900

    style: 댓글 데이터 별도 객체로 저장

--- my-app/src/chapter_05/CommentList.jsx
+++ my-app/src/chapter_05/CommentList.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+  {
+    name: "이인제",
+    comment: "안녕하세요, 소플입니다.",
+  },
+  {
+    name: "유재석",
+    comment: "리액트 재미있어요~!",
+  },
+  {
+    name: "강민경",
+    comment: "저도 리액트 배워보고 싶어요!!",
+  },
+];
+
 function CommentList(props) {
   return (
     <div>
-      <Comment name={"김철수"} comment={"안녕하세요, 수플입니다."} />
-      <Comment name={"콩진호호"} comment={"안녕하세요. 안녕하세요요."} />
+      {comments.map((comment) => {
+        return <Comment name={comment.name} comment={comment.comment} />;
+      })}
     </div>
   );
 }
